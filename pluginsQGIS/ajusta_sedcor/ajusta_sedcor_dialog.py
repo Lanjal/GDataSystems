f8d53c5fbc70bb1821790267ced0604432064324
André Luiz Lima Costa <magcrystals@gmail.com>
2023-04-17 14:06:56 -0300

    Update ajusta_sedcor_dialog.py

--- pluginsQGIS/ajusta_sedcor/ajusta_sedcor_dialog.py
+++ pluginsQGIS/ajusta_sedcor/ajusta_sedcor_dialog.py
@@ -15,9 +15,8 @@
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   it under the terms of the is licensed under the BSD 3-Clause "New" or *
+ *   "Revised" License.
  *                                                                         *
  ***************************************************************************/
 """
